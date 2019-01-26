2807736d871b8cd6b7815cf02b3e419f7f92711e
T. Thyer <1972488+tthyer@users.noreply.github.com>
2019-01-26 15:32:02 -0800

    Install iTerm

--- install_software.sh
+++ install_software.sh
@@ -2,10 +2,21 @@
 
 set -e
 
+if [[ ! -d "/Applications/iTerm.app" ]]; then
+    echo "Installing iTerm"
+    curl -o "iterm.zip" -L "https://iterm2.com/downloads/stable/latest"
+    unzip "iterm.zip"
+    sudo mv "iTerm.app" "/Applications"
+    rm "iterm.zip"
+    echo 'Done.\n'
+fi
+
+exit
+
 # install homebrew
 echo "Installing Homebrew..."
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-echo "Done.\n"
+echo 'Done.\n'
 
 # install Java 8 (through a download)
 echo "Have you installed Java 8?"
@@ -18,15 +29,15 @@ done
 
 echo "Installing Scala 2.11..."
 brew install scala@2.11
-echo "Done.\n"
+echo 'Done.\n'
 
 echo "Installing Apache Spark..."
 brew install apache-spark
-echo "Done. Installed $(head -n 1 $SPARK_HOME/RELEASE).\n"
+echo 'Done. Installed $(head -n 1 $SPARK_HOME/RELEASE).\n'
 
 echo "Installing Sublime..."
 brew cask install sublime-text
-echo "Done.\n"
+echo 'Done.\n'
 
 # echo "Would you liked to install awscli?"
 # select yn in "Yes" "No"; do
