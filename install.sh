25ad185e78f29278d6b6ef3e93c679f6186ada4f
T. Thyer <1972488+tthyer@users.noreply.github.com>
2019-01-26 15:07:31 -0800

    Make install script smarter and more sensitive

--- install.sh
+++ install.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
 
-ln -sv ".bash_profile" ~
-ln -sv ".vimrc" ~
-ln -sv ".gitconfig" ~
-ln -sv ".gitignore_global" ~
+files=(
+    ".bash_profile"
+    ".vimrc"
+    ".gitconfig"
+    ".gitignore_global"
+    )
+
+for dotfile in "${files[@]}"
+do
+    if [[ -f "$HOME/$dotfile" ]]; then
+      rm "$HOME/$dotfile"
+      rm "$HOME/$dotfile.bck"
+    fi
+    ln -sv "$(pwd)/$dotfile" "$HOME/$dotfile"
+done
+
+# ln -sv ".bash_profile" ~
+# ln -sv ".vimrc" ~
+# ln -sv ".gitconfig" ~
+# ln -sv ".gitignore_global" ~
