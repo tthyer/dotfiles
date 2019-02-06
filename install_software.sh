cf28c3830889976ec5470e9bc6802263883a5aaa
T. Thyer <tess.thyer@gmail.com>
2019-02-06 10:26:03 -0800

    Fix jdk install; switch spark from homebrew install to manual; install other command line tools through homebrew

--- install_software.sh
+++ install_software.sh
@@ -26,39 +26,61 @@ fi
 # JDK
 if [[ -z "$(ls /Library/Java/JavaVirtualMachines/ | grep jdk)" ]]; then
     echo "Installing JDK 8..."
-    #download
-    curl -v -j -k -L -H "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u112-b15/jre-8u112-macosx-x64.dmg > jre-8u112-macosx-x64.dmg
-    #mount
-    hdiutil attach jre-8u112-macosx-x64.dmg
-    #install
-    sudo installer -pkg /Volumes/Java\ 8\ Update\ 112/Java\ 8\ Update\ 112.app/Contents/Resources/JavaAppletPlugin.pkg -target /
-    #cleanup
-    diskutil umount /Volumes/Java\ 8\ Update\ 112 
-    rm jre-8u112-macosx-x64.dmg
+    # download with options junk-session-cookies, insecure, redirect to new location, header specified
+    curl --j -k -L -H "Cookie: oraclelicense=accept-securebackup-cookie" \
+        -o jdk-8u201-macosx-x64.dmg \
+        https://download.oracle.com/otn-pub/java/jdk/8u201-b09/jdk-8u201-macosx-x64.dmg
+    # mount
+    hdiutil attach jdk-8u201-macosx-x64.dmg
+    # install
+    sudo installer -pkg /Volumes/Java\ 8\ Update\ 201/Java\ 8\ Update\ 201.app/Contents/Resources/JavaAppletPlugin.pkg -target /
+    # cleanup
+    diskutil umount /Volumes/Java\ 8\ Update\ 201
+    rm jdk-8u201-macosx-x64.dmg
 else
     echo "JDK 8 is already installed, skipping."
 fi
 
-# SCALA
-if [[ -z "$(brew list | grep scala)" ]]; then
-    echo "Installing Scala 2.11..."
-    brew install scala@2.11
-    echo 'Done.\n'
-else
-    echo "Scala 2.11 is already installed, skipping."    
-fi
+# Things I like to install through Homebrew
+# apache-spark is available but need to control the version
+brew_things=( scala@2.11 sbt tree gnu-sed bash-completion python maven jq parquet-tools )
+for i in "${brew_things[@]}"
+do
+    if [[ -z "$(brew list | grep $i)" ]]; then
+        echo "Installing $i through Homebrew..."
+        brew install $i
+        echo 'Done.\n'
+    else
+        echo "$i is already installed, skipping."
+    fi
+done
 
 # SPARK
-if [[ -z "$(brew list | grep spark)" ]]; then
-    echo "Installing Apache Spark..."
-    brew install apache-spark
-    echo 'Done. Installed $(head -n 1 $SPARK_HOME/RELEASE).\n'
+# if [[ -z "$(brew list | grep spark)" ]]; then
+#     echo "Installing Apache Spark..."
+#     brew install apache-spark
+#     echo 'Done. Installed $(head -n 1 $SPARK_HOME/RELEASE).\n'
+# else
+#     echo "Spark is already installed, skipping."    
+# fi
+if [[ -z "$(which spark-submit)" ]]; then
+    echo "Installing Apache Spark 2.3.2..."
+    # download with options junk-session-cookies, insecure, redirect to new location
+    curl -j -k -L -o spark-2.3.2-bin-hadoop2.7.tgz https://www-us.apache.org/dist/spark/spark-2.3.2/spark-2.3.2-bin-hadoop2.7.tgz
+    # untar to root
+    echo "You will be prompted for your password to unpack spark at root."
+    sudo tar -zxvf spark-2.3.2-bin-hadoop2.7.tgz -C /
+    # cleanup
+    rm spark-2.3.2-bin-hadoop2.7.tgz
+
+    echo "Spark is installed; remember to add /spark-2.3.2-bin-hadoop2.7/bin to PATH."
+    # TODO check whether it is added and if not then add export line in ~/.bash_profile and source it
 else
-    echo "Spark is already installed, skipping."    
+    echo "Spark is already installed, skipping."
 fi
 
 # SUBLIME
-if [[ -z "$(brew list | grep sublime)" && ! -d /Applications/Sublime\ Text.app/ ]]; then
+if [[ -z "$(brew list | grep sublime-text)" && ! -d /Applications/Sublime\ Text.app/ ]]; then
     echo "Installing Sublime..."
     brew cask install sublime-text
     echo 'Done.\n'
@@ -66,12 +88,10 @@ else
     echo "Sublime is already installed, skipping."    
 fi
 
-# echo "Would you liked to install awscli?"
-# select yn in "Yes" "No"; do
-#     case $yn in
-#         Yes ) "Installing awscli via brew..."; brew install awscli; echo "Done.\n"; break;;
-#         No ) echo "Skipping install of awscli.\n";;
-#     esac
-# done
-
-# add a section here for 
+if [[ -z "$(brew list | grep awscli)" ]]; then 
+    echo "Installing awscli through Homebrew..."
+    brew install awscli
+    echo 'Done.\n'
+else
+    echo "awscli is already installed, skipping."
+fi
