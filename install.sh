9957bac42842fc2f6218f383c3e866f0fe6a7631
T. Thyer <tess.thyer@gmail.com>
2019-02-16 12:47:35 -0800

    Clean up install.sh

--- install.sh
+++ install.sh
@@ -11,12 +11,6 @@ for dotfile in "${files[@]}"
 do
     if [[ -f "$HOME/$dotfile" ]]; then
       rm "$HOME/$dotfile"
-      rm "$HOME/$dotfile.bck"
     fi
     ln -sv "$(pwd)/$dotfile" "$HOME/$dotfile"
 done
-
-# ln -sv ".bash_profile" ~
-# ln -sv ".vimrc" ~
-# ln -sv ".gitconfig" ~
-# ln -sv ".gitignore_global" ~
