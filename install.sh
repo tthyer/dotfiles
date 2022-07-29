d3aa2fc4525b9fe32ce99f8e1d17bdc9e53f83b7
T. Thyer <1972488+tthyer@users.noreply.github.com>
2022-07-28 19:39:34 -0700

    Improve test to detect whether homebrew is installed

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ else
 fi
 
 # HOMEBREW
-if [[ -z "$(which brew)" ]]; then
+if ! command -v brew &> /dev/null; then
   echo "Installing Homebrew..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/tessthyer/.bash_profile
