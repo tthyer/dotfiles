7d1d6f536657e52ff1935f2c8ac082d0871bab21
T. Thyer <1972488+tthyer@users.noreply.github.com>
2022-08-05 13:17:23 -0700

    Fix java and spark config

--- spark.sh
+++ spark.sh
@@ -1,19 +1,15 @@
 #!/usr/bin/env bash
 
 java_path=$(brew --prefix openjdk@11)
-echo "ln -sfn ${java_path}/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-11.jdk" > ~/.config-java-spark.sh
+jdk_link_path=/Library/Java/JavaVirtualMachines/openjdk-11.jdk
+echo "! test -h ${jdk_link_path} && sudo ln -sfn ${java_path}/libexec/openjdk.jdk ${jdk_link_path}" > ~/.config-java-spark.sh
 echo "export PATH=${java_path}/bin:${PATH}" >> ~/.config-java-spark.sh
 echo "export CPPFLAGS=-I${java_path}/include" >> ~/.config-java-spark.sh
-echo "export JAVA_HOME=$(/usr/libexec/java_home)" >> ~/.config-java-spark.sh
-echo "echo JAVA_HOME=$JAVA_HOME"
-echo "launchctl setenv JAVA_HOME ${JAVA_HOME}" >> ~/.config-java-spark.sh
-
-spark_home=$(brew --prefix apache-spark)
-echo "export SPARK_HOME=${spark_home}" >> ~/.config-java-spark.sh 
-echo "export PATH=${spark_home}/bin:$PATH" >> ~/.config-java-spark.sh
+java_home=$(/usr/libexec/java_home)
+echo "export JAVA_HOME=${java_home}" >> ~/.config-java-spark.sh
+echo "launchctl setenv JAVA_HOME ${java_home}" >> ~/.config-java-spark.sh
 
 ## configure
 chmod +x ~/.config-java-spark.sh
 COMMAND='source ~/.config-java-spark.sh'
 grep -qF -- "$COMMAND" ~/.bash_profile || echo "$COMMAND" >> ~/.bash_profile
-
